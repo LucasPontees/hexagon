8940b1a4091f3a20d2376f1f9de2b20890a00cc1
LucasPontees <eulpontes@gmail.com>
2024-10-25 00:45:58 -0400

    remoção da camada de interface

--- src/application/interfaces/product-repository.interface.ts
+++
@@ -1,6 +0,0 @@
-import { Product } from '../../infrastructure/entities/product.entity';
-
-export interface ProductRepository {
-  create(product: Product): Promise<Product>;
-  findAll(): Promise<Product[]>;
-}
